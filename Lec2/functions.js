cbe678ae5f7dd40396bb8f17f91bc2edbf264889
Manthan <mnthnarora@gmail.com>
2021-05-27 02:00:15 +0530

    Functions

---
+++ Lec2/functions.js
@@ -0,0 +1,16 @@
+//Function body
+//function sayHi(){
+  //  console.log("Function says hi")
+
+//}
+//functions are variable
+
+//Function call
+//sayHi();
+
+//console.log(sayHi());
+let sayHi=function(name){
+    console.log(name +"I am a function stored in SayHi")
+}
+//console.log(sayHi);
+sayHi("Steve"); 
